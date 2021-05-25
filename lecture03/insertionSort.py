7b4a27de8caedb5cb6c5ac657cc3f12996b81f80
dongmyeong.lee <dom9316@gmail.com>
2021-05-26 00:40:00 +0900

    Insertion Sort

---
+++ lecture03/insertionSort.py
@@ -0,0 +1,67 @@
+"""
+Lecture 3: Insertion Sort & Merge Sort
+
+Insertion Sort
+- Time Complexity: 𝛩(n^2)
+- Space Complexity: 𝛩(1)
+"""
+
+from typing import List
+
+
+def insertion_sort(arr: List[int]) -> List[int]:
+    """
+       𝛩(n^2) Compares, 𝛩(n^2) Swaps
+    """
+    for i in range(1, len(arr)):
+        for j in range(i):
+            if arr[i - j] < arr[i - j - 1]:
+                arr[i - j], arr[i - j - 1] = arr[i - j - 1], arr[i - j]
+
+    return arr
+
+
+def bianry_insertion_sort(arr: List[int]) -> List[int]:
+    """
+       - 𝛩(nlogn) Compares, 𝛩(n^2) Swaps
+    """
+    for i in range(1, len(arr)):
+        key = arr[i]
+        swap_index = binary_search(arr[:i], 0, i, key)
+
+        del arr[i]
+        arr.insert(swap_index, key)
+
+    return arr
+
+
+def binary_search(arr, low, high, key):
+    if high - low == 1:
+        if arr[low] < key:
+            return high
+        else:
+            return low
+
+    else:
+        mid = (high + low) // 2
+        if arr[mid] == key:
+            return mid
+        elif arr[mid] > key:
+            return binary_search(arr, low, mid, key)
+        else:
+            return binary_search(arr, mid, high, key)
+
+
+def main():
+    arr = [1, 5, 2, 6, 3, 7, 4]
+    for _ in range(10):
+        arr.extend(arr)
+
+    print(insertion_sort(arr))
+    # print(bianry_insertion_sort(arr))
+
+
+if __name__ == "__main__":
+    import profile
+
+    profile.run("main")
---
+++ lecture03/lecture03.md
@@ -0,0 +1,17 @@
+# Lecture03
+## Insertion Sort & Merger Sort
+
+### Insertion Sort
+- Time Complexity: 𝛩(n^2)
+- Space Complexity: 𝛩(1)
+
+1. Insertion sort
+   - 𝛩(n^2) Compares, 𝛩(n^2) Swaps
+     <br/><br/>
+     
+2. Binary Insertion Sort
+   - 𝛩(nlogn) Compares, 𝛩(n^2) Swaps
+
+### Merge Sort (divide & conquer)
+- Time Complexity: 𝛩(nlogn)
+- Space Complexity: 𝛩(n)
